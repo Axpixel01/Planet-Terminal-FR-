3b82da1f09354e0b3fde62f12c5448cb43e7e0f9
Axpixel01 <162464654+Axpixel01@users.noreply.github.com>
2024-06-28 00:53:59 +0200

    Version 0.0.9

--- planet_terminal english.py
+++ planet_terminal english.py
@@ -1,7 +1,7 @@
-# Welcome to Planet Terminal Version 0.0.8. Here you can execute some commands which are... Interesting...
+# Welcome to Planet Terminal Version 0.0.9. Here you can execute some commands which are... Interesting...
 # Latest update log: Added the /js_version command
 # If there are any bugs, contact me on Discord: axpixel01
-version = ("Version 0.0.8")
+version = ("Version 0.0.9")
 commande = input("Welcome to Planet Terminal {}. What command will you execute ? Indicate it right here:".format(version))
 
 if commande == ("/tp"):
@@ -48,8 +48,20 @@ elif commande == ("/destroy"):
     print("Ok, {} destroyed.".format(question8))
 elif commande == ("/js_version"):
     print("The JavaScript version is available on GitHub (French Only) : https://github.com/Axpixel01/Planet-Terminal-FR-")
+elif commande == ("/god"):
+    question9 = input("Who do you want to turn into a god?")
+    print("Alright. {} has been selected".format(question9))
+elif commande == ("/action"):
+    question10 = input("What do you want to do ?")
+    print("Alright. Action completed.")
+elif commande == ("/amongus"):
+    print("SUS")
+elif commande == ("/noob"):
+    print(";-;")
+elif commande == ("/weapon"):
+    print("Here, your weapon.")
 elif commande == ("/help"):
-    print("Here is the help menu: \n/tp: Allows you to teleport to a country. \n/nuke: Allows you to destroy an area. \n/mute: Allows mute. \n/kill: Allows you to kill someone. \n/play_warframe: Allows you to play Warframe. \n/language: Allows you to change the language. \n/give: Allows you to give anything. \n/resurrect: Allows you to resurrect whatever you want. \n/trap: Allows you to trap anything. \n/ping: Allows you to ping someone. \n/destroy: Allows you to destroy whatever you want. \n/help: Allows you to have all the commands.")
+    print("Here is the help menu: \n/tp: Allows you to teleport to a country. \n/nuke: Allows you to destroy an area. \n/mute: Allows mute. \n/kill: Allows you to kill someone. \n/play_warframe: Allows you to play Warframe. \n/language: Allows you to change the language. \n/give: Allows you to give anything. \n/resurrect: Allows you to resurrect whatever you want. \n/trap: Allows you to trap anything. \n/ping: Allows you to ping someone. \n/destroy: Allows you to destroy whatever you want. \n/god : Allows you to transform someone into a god. \n/action : Allows you to perform an action. \n/amongus : SUS. \n/noob : noob. \n/weapon : Allows you to have a weapon. \n/help: Allows you to have all the commands.")
 elif commande == (""):
     print("Error n°101: No Arguments were put.")
 elif commande == (" "):
--- planet_terminal.py
+++ planet_terminal.py
@@ -1,7 +1,7 @@
-# Bienvenue sur le Planet Terminal Version 0.0.8 . Ici, vous pouvez éxécuter quelques commandes qui sont... Intéréssantes...
-# Dernier log de màj : Ajout de la commande /version_js
+# Bienvenue sur le Planet Terminal Version 0.0.9 . Ici, vous pouvez éxécuter quelques commandes qui sont... Intéréssantes...
+# Dernier log de màj : Ajout de la commande /dieu, /action, /amongus, /noob, /arme, /ref, /envie_de...
 # Si il y à des bugs, contactez moi sur Discord : axpixel01
-version = ("Version 0.0.8")
+version = ("Version 0.0.9")
 commande = input("Bienvenue sur le Planet Terminal {}. Quelle commande allez-vous éxécuter ? Indiquez là juste ici :".format(version))
 
 if commande == ("/tp"):
@@ -48,8 +48,24 @@ elif commande == ("/destroy"):
     print("Ok, {} détruit(e).".format(question8))
 elif commande == ("/version_js"):
     print("La version JavaScript est là pour une meilleure version en ligne grâce au HTML. Elle est disponible sur GitHub : https://github.com/Axpixel01/Planet-Terminal-FR-")
+elif commande == ("/dieu"):
+    question9 = input("Qui voulez-vous transformer en dieu ?")
+    print("Très bien. {} à été séléctionné(e)".format(question9))
+elif commande == ("/action"):
+    question10 = input("Que voulez-vous faire ?")
+    print("Très bien. Action effectuée.")
+elif commande == ("/amongus"):
+    print("SUS")
+elif commande == ("/noob"):
+    print(";-;")
+elif commande == ("/arme"):
+    print("Tiens, ton arme.")
+elif commande == ("/ref"):
+    print("Y'en à trop...")
+elif commande == ("/envie_de..."):
+    print("Quoi ?! *TAP* Comment ça mon reuf ?!")
 elif commande == ("/help"):
-    print("Voici le menu d'aide : \n/tp : Permet de se téléporter dans un pays. \n/nuke : Permet d'anéantir une zone. \n/mute : Permet de mute. \n/kill : Permet d'élminier quelqu'un. \n/play_warframe : Permet de jouer à Warframe. \n/langue : Permet de changer la langue. \n/give : Permet de se donner n'importe quoi. \n/ressusciter : Permet de réssusciter ce que vous voulez. \n/piège : Permet de piéger n'importe quoi. \n/ping : Permet de ping quelqu'un. \n/destroy : Permet de détruire ce que vous souhaitez. \n/help : Permet d'avoir toutes les commandes.")
+    print("Voici le menu d'aide : \n/tp : Permet de se téléporter dans un pays. \n/nuke : Permet d'anéantir une zone. \n/mute : Permet de mute. \n/kill : Permet d'élminier quelqu'un. \n/play_warframe : Permet de jouer à Warframe. \n/langue : Permet de changer la langue. \n/give : Permet de se donner n'importe quoi. \n/ressusciter : Permet de réssusciter ce que vous voulez. \n/piège : Permet de piéger n'importe quoi. \n/ping : Permet de ping quelqu'un. \n/destroy : Permet de détruire ce que vous souhaitez. \n/dieu : Permet de transformer quelqu'un en dieu. \n/action : Permet d'effectuer une action. \n/amongus : SUS. \n/noob : noob. \n/arme : Permet d'avoir une arme. \n/ref : Ouais j'ai plus trop d'inspi là... \n/envie_de... : HEY COMMENT çA ?! \n/help : Permet d'avoir toutes les commandes.")
 elif commande == (""):
     print("Erreur n°101 : Aucun Arguments n'a été mis.")
 elif commande == (" "):
